0f43442f1e665c1a876ef531609dbe20a0a79ca1
이동아 <christar99@naver.com>
2023-06-30 16:37:28 +0900

    이력서 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,20 @@ export default function index() {
                 <Email>이메일: christar99@naver.com</Email>
                 <Phone>연락처: 010-2509-8547</Phone>
                 <Introduce>
-                    스타트업에서 웹서비스의 개발/유지보수를 담당했으며, 필요에 따라 기획단계에서 회의도 참여하여
-                    일정이나 가능여부등을 조율하기도 했습니다. 저는 비즈니스가 안정적으로 잘 구현되는 것이 최우선이라
-                    생각합니다. 장애대응을 원할하게 하기위하여 에러트래킹 툴의 기능들을 활용하여 원인을 쉽게 추적할 수
-                    있게 하고, 웹사이트를 리팩토링하여 반복되는 구문은 컴포넌트화/모듈화 시켜 유지보수 용이하게 작업한
-                    적 있습니다.
+                    <ContentUL>
+                        <ContentList>
+                            스타트업에서 웹서비스의 개발/유지보수를 담당했습니다. 웹사이트를 리팩토링하여 반복되는
+                            구문은 컴포넌트화/모듈화 시켜 유지보수 용이하게 작업한 적 있습니다.
+                        </ContentList>
+                        <ContentList>
+                            저는 비즈니스가 안정적으로 잘 구현되는 것이 최우선이라 생각하므로 장애대응을 원할하게 하기
+                            위하여 에러트래킹 툴의 기능들을 활용하여 원인을 쉽게 추적할 수 있게 했습니다.
+                        </ContentList>
+                        <ContentList>
+                            유저와 인터렉션하여 유저의 경험을 주는 웹을 개발하는 것을 즐겨합니다. 유저가 주체가 되어
+                            window10의 기본적인 기능을 사용할 수 있는 웹사이트를 만든적이 있습니다.
+                        </ContentList>
+                    </ContentUL>
                 </Introduce>
                 <Divider />
                 <SectionTop>
@@ -40,7 +49,8 @@ export default function index() {
                             <Emphasis> 번들사이즈 약 30%감소</Emphasis>
                         </ContentList>
                         <ContentList>
-                            에러트래킹 Sentry 개선, 크로스브라우징 대응. Issue Grouping 으로 필요한 알람만 설정.
+                            에러트래킹 툴 Sentry 개선, 중요한 에러는 수집하여 예상되는 에러 예방. 비슷한 에러는 Issue
+                            Grouping하고, 필요한 알람만 설정.
                         </ContentList>
                     </ContentUL>
                     <ProejctName>
@@ -50,11 +60,7 @@ export default function index() {
                         <ContentList>웹사이트의 전반적인 부분을 첫 구축단계에서부터 개발(90% 이상 기여)</ContentList>
                         <ContentList>상태관리, api요청 모듈화, 컴포넌트 구성 등의 아키텍처 설계</ContentList>
                         <ContentList>
-                            유저입장에서 예약업데이트 현황을 실시간으로 보기위해 WebSocket활용. 예약페이지 진입 시
-                            Socket.io에 접속하여 페이지 이탈 전까지 지속적인 양방향 통신
-                        </ContentList>
-                        <ContentList>
-                            하자등록할 때 사진, 동영상 첨부 가능. 파일은 Blob객체로 변환 후 서버에 저장
+                            하자등록할 때 사진, 동영상 업로드 기능 구현. 파일은 Blob객체로 변환 후 스토리지에 저장.
                         </ContentList>
                     </ContentUL>
                 </SectionContent>
@@ -67,21 +73,20 @@ export default function index() {
                 <SectionContent style={{ marginBottom: "40px" }}>
                     <ContentUL>
                         <ContentList>
-                            신입 준비 때 만들었던 LOLCord를 개선하여 만든 웹. Next.js, Typescript, Tanstack Query, jotai
-                            등 도입.
+                            신입 준비 때 만들었던 LOLCord(게임 LOL의 정보를 볼수있는 웹사이트)를 개선하여 만든 웹.
+                            이전에는 React만을 사용했으나, Next.js, Typescript, Tanstack Query, jotai 등 도입.
                         </ContentList>
                         <ContentList>
                             <Emphasis>Next.js의 Pre-render</Emphasis>를 통해 페이지 진입속도 상승(첫페이지 1.5s →
                             0.5s미만, 챔피언페이지 7s이상 → 0.5s 미만)
                         </ContentList>
                         <ContentList>
-                            최적화 작업. <Emphasis>메모이제이션</Emphasis>을 활용해, 불필요연산 감소. 알고리즘 개선.{" "}
-                            <Emphasis>Nextjs 이미지 최적화</Emphasis>
+                            <Emphasis>메모이제이션</Emphasis>을 활용해 불필요한 연산 감소시킴. 알고리즘 개선과{" "}
+                            <Emphasis>이미지 최적화</Emphasis>를 통해 성능을 향상시킴.
                         </ContentList>
                         <ContentList>
-                            패치노트 JSON파일 사용 시, <Emphasis>CORS 에러 발생</Emphasis>으로 express.js의 서버 구축 때
-                            CORS미들웨어를 활용(aws EC2, route53으로 배포). 프론트단에서 구축한 서버의 API를 호출하여
-                            해결.
+                            API 요청시 발생하는 CORS를 해결하기 위해 express.js 서버를 중간에 두고 우회하는 방식을 적용.
+                            서버 배포는 AWS 스택을 활용함 (EC2, Route53)
                         </ContentList>
                     </ContentUL>
                 </SectionContent>
@@ -94,11 +99,11 @@ export default function index() {
                     <ContentUL>
                         <ContentList>웹사이트를 window10 desktop 처럼 사용할 수 있게 만든 페이지.</ContentList>
                         <ContentList>
-                            사용자 계정은 cookie, 각종 설정, 배경화면 이미지, 저장한 파일의 uuid등은 localStorage에 저장
-                            → 브라우저가 종료되어도, 실행 시 유저가 저장했던 설정들을 자동으로 셋팅.
+                            사용자 계정은 Cookie에 저장하고 각종 설정 및 이미지, 파일의 uuid등은 localStorage에
+                            저장함으로써 브라우저가 종료되어도 유저가 설정했던 세팅을 자동으로 불러옴
                         </ContentList>
                         <ContentList>테마설정으로 다크모드, 글꼴, 해상도 설정 구현</ContentList>
-                        <ContentList>textArea로 메모장 기능 구현, javascript의 canvasAPI로 그림판구현</ContentList>
+                        <ContentList>textArea로 메모장 기능 구현, webAPI의 canvasAPI로 그림판구현</ContentList>
                         <ContentList>
                             <Emphasis>메모장과 그림판을 저장하면 AWS S3에 담아두어</Emphasis>, 추후에도 열람/수정 가능
                         </ContentList>
@@ -125,7 +130,7 @@ export default function index() {
                         </ContentUL>
                         <SkillCategory>Web</SkillCategory>
                         <ContentUL>
-                            <ContentList>네트워크 통신에 대한 전반적인 구조를 이해하고 있습니다.</ContentList>
+                            <ContentList>HTTP 네트워크 통신에 대한 전반적인 구조를 이해하고 있습니다.</ContentList>
                             <ContentList>SEO(검색엔진 최적화)경험이 있습니다.</ContentList>
                             <ContentList>웹표준, 웹접근성에 대한 이해를 하고, 준수하려 노력합니다.</ContentList>
                         </ContentUL>
@@ -150,7 +155,7 @@ export default function index() {
                         <ContentUL>
                             <ContentList>AWS EC2, nginx등을 이용하여 웹사이트를 배포할 수 있습니다.</ContentList>
                             <ContentList>
-                                git으로 버전을 관리하고, Github로 원격저장소에 업로드하여 다른 개발자들과 공유할 수
+                                Git으로 버전을 관리하고, Github로 원격저장소에 업로드하여 다른 개발자들과 공유할 수
                                 있습니다.
                             </ContentList>
                         </ContentUL>
@@ -171,10 +176,10 @@ export default function index() {
                                 포트폴리오 MyLittleSpace:{" "}
                                 <Link href="https://mylittlespace.site">https://mylittlespace.site</Link>
                             </ContentList>
-                            <ContentList>
+                            {/* <ContentList>
                                 이력서 웹버전(내용은 본 pdf파일과 같습니다):{" "}
                                 <Link href="https://resume.dong-a.me">https://resume.dong-a.me</Link>
-                            </ContentList>
+                            </ContentList> */}
                         </ContentUL>
                     </SectionContent>
                 </OtherSection>
